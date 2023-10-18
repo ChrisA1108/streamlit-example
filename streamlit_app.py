37fd1637f5a39a0b159be4f649d26fd8db8d5824
Christopher Alvarado <104535558+ChrisA1108@users.noreply.github.com>
2023-10-18 13:55:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,81 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import requests
+import re
+import json
 
-"""
-# Welcome to Streamlit!
+# Function to parse HBO Max JSON
+def parse_hbom(hbom_json):
+    result = []
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+    for components in hbom_json['components']:
+        supplier = components.get('supplier', {}).get('name', '')
+        entry = {
+            'supplier': supplier,
+            'description': components.get('description', ''),
+            'referenceURL': None,
+            'cve_entries': []  # Initialize an empty list for CVE entries
+        }
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+        if 'externalReferences' in components:
+            for i in components['externalReferences']:
+                entry['referenceURL'] = i['url']
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+                base_url = "https://services.nvd.nist.gov/rest/json/cves/2.0"
+                params = {'keywordSearch': entry['description']}
+                response = requests.get(base_url, params=params)
 
+                if response.status_code == 200:
+                    data = response.json()
+                    if data.get('totalResults', 0) > 0:
+                        cve_entries = data.get("vulnerabilities", {})
+                        for cve_entry in cve_entries:
+                            cve_id = cve_entry.get('cve', {}).get('id', "")
+                            cve_descriptions = cve_entry.get('cve', {}).get('descriptions', {})
+                            cve_references = cve_entry.get('cve', {}).get("references", {})
+                            cve_weaknesses = cve_entry.get('cve', {}).get('weaknesses', {})
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+                            cpe_string = cve_entry.get('cve', {}).get("configurations", {})[0].get('nodes', {})[0].get(
+                                'cpeMatch', {})[0].get('criteria', "")
+                            pattern = r'(.*?):(.*?):(.*?):' + re.escape(supplier.lower()) + r':'
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+                            if re.findall(pattern, cpe_string):
+                                cve_info = {
+                                    'CVE': cve_id,
+                                    'References': [],
+                                    'Descriptions': []
+                                }
 
-    points_per_turn = total_points / num_turns
+                                for reference in cve_references:
+                                    reference_url = reference.get("url", "")
+                                    reference_source = reference.get("source", "")
+                                    cve_info['References'].append({
+                                        'Source': reference_source,
+                                        'URL': reference_url
+                                    })
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+                                for description in cve_descriptions:
+                                    description_text = description.get('value', "")
+                                    description_lang = description.get('lang', "")
+                                    cve_info['Descriptions'].append({
+                                        'Language': description_lang,
+                                        'Description': description_text
+                                    })
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+                                entry['cve_entries'].append(cve_info)  # Append CVE info to the entry
+        result.append(entry)
+
+    return result
+
+# Streamlit app
+st.title("HBO Max JSON Parser")
+
+uploaded_file = st.file_uploader("Upload your HBO Max JSON file", type=["json"])
+
+if uploaded_file is not None:
+    try:
+        hbom_json = json.load(uploaded_file)
+        parsed_data = parse_hbom(hbom_json)
+        st.write("Parsed Data:")
+        st.write(parsed_data)
+    except json.JSONDecodeError:
+        st.error("Invalid JSON file. Please upload a valid JSON file.")
