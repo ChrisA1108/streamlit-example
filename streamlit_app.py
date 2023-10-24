ab46a88e5f111ed47f74696eaec95c25123f8272
Christopher Alvarado <104535558+ChrisA1108@users.noreply.github.com>
2023-10-24 15:02:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,49 +5,58 @@ import json
 
 def process_hbom(hbom_json):
     for components in hbom_json['components']:
-        supplier = components.get('supplier', {}).get('name', '')
+    supplier = components.get('supplier', {}).get('name', '')
 
-        if 'externalReferences' in components:
-            cves_found = False  # Track if any CVEs were found for this component
-            # Check if the component has external references
-            for i in components['externalReferences']:
-                referenceURL = i['url']
-                description = components['description']
-                #testing
-                if description.split:
-                    description = description.split()[0]
+    if 'externalReferences' in components:
+        # Check if the component has external references
+        for i in components['externalReferences']:
+            referenceURL = i['url']
+            description = components['description']
+            keywords = description.split()
 
-                # Print supplier and description
-                st.write(f'{supplier}: {description}')
-                st.write(f'referenceURL: {referenceURL}')
-                st.write('')
+            keywordFound = False
 
-                # Define the base URL for querying vulnerabilities
-                base_url = "https://services.nvd.nist.gov/rest/json/cves/2.0"
+            # Print supplier and description
+            st.write(f'{supplier}: {description}')
+            st.write(f'referenceURL: {referenceURL}')
+            st.write('')
 
-                # Prepare the search parameters using the component description
-                params = {'keywordSearch': f"{description}"}
+            # Define the base URL for querying vulnerabilities
+            base_url = "https://services.nvd.nist.gov/rest/json/cves/2.0"
+
+            # Prepare the search parameters using the component description
+
+            for keyword in keywords:
+                if keyword.lower() == supplier.lower():
+                    # Skip keyword search when it's the same as the supplier
+                    continue
+
+                params = {'keywordSearch': f"{keyword}"}
                 response = requests.get(base_url, params=params)
 
                 if response.status_code == 200:
                     data = response.json()
-
                     if data.get('totalResults', '') > 0:
-                        
                         cve_entries = data.get("vulnerabilities", {})
-
+                        keywordFound = False
+                        # print(keyword)
                         for entry in cve_entries:
                             cve_id = entry.get('cve', {}).get('id', "")
                             cve_descriptions = entry.get('cve', {}).get('descriptions', {})
                             cve_weaknesses = entry.get('cve', {}).get('weaknesses', {})
                             cve_references = entry.get('cve', {}).get("references", {})
 
-                            cpe_string = entry.get('cve', {}).get("configurations", {})[0].get('nodes', {})[0].get('cpeMatch', {})[0].get('criteria', "")
+                            cpe_string = \
+                                entry.get('cve', {}).get("configurations", [{}])[0].get('nodes', [{}])[0].get(
+                                    'cpeMatch', [{}])[
+                                    0].get('criteria', "")
+                            supplier = supplier.split(" ")[0]
+
                             pattern = r'(.*?):(.*?):(.*?):' + re.escape(supplier.lower()) + r':'
 
                             # Use regular expressions to find a match in the CPE string
                             if re.findall(pattern, cpe_string):
-                                cves_found = True  # CVEs found for this component
+                                keywordFound = True
                                 if cve_id and cve_descriptions:
                                     # Print CVE details
                                     st.write(f'CVE: {cve_id}')
@@ -55,12 +64,12 @@ def process_hbom(hbom_json):
                                     for reference in cve_references:
                                         reference_url = reference.get("url", "")
                                         reference_source = reference.get("source", "")
-                                        st.write(f'References - Source: {reference_source}, URL: {reference_url}')
+                                        print(f'References - Source: {reference_source}, URL: {reference_url}')
 
                                     for description in cve_descriptions:
                                         description_text = description.get('value', "")
                                         description_lang = description.get('lang', "")
-                                        st.write(f'CVE description in {description_lang}: {description_text}')
+                                        print(f'CVE description in {description_lang}: {description_text}')
                                 elif cve_id and not cve_descriptions:
                                     # Print CVE without descriptions
                                     st.write(f'CVE: {cve_id}')
@@ -74,16 +83,9 @@ def process_hbom(hbom_json):
                                                 cwe_name = cwe.get('value', "")
                                                 # Print CWE information
                                                 st.write(f'CWE for {cve_id}: {cwe_name}, Source: {cwe_source}')
-                             
-
-            if not cves_found:
-                st.write("No current CVEs for this component")
-        else:
-            supplier_name = components.get('supplier', {}).get('name', "")
-            description_base = components.get('description', "")
-
-            # Print supplier and description if there are no external references
-            st.write(f'{supplier_name}: {description_base}')
+                                st.write('')
+                if keywordFound:
+                    break
 
 st.title("HBOM Component Processing")
 
