5bc83104f985fe3c3cc73022a6c099f93b726faf
Christopher Alvarado <104535558+ChrisA1108@users.noreply.github.com>
2023-11-09 07:41:57 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,76 +5,73 @@ import streamlit as st
 import json
 
 def process_hbom(json_data):
-    for components in json_data['components']:
-        keyCnt = 0 # count for keeping track of keys
-        cve_found = False  # Flag to check if CVEs were found
+    for components in HBOM['components']:
         searchByKeyword = True
         supplier = components.get('supplier', {}).get('name', '')
         description = components['description']
-
+    
         # Print supplier and description
         st.write(f'# {supplier}: {description}')
         if 'externalReferences' in components:
             # Check if the component has external references
             for i in components['externalReferences']:
                 referenceURL = i['url']
-
+    
             st.write(f'referenceURL: {referenceURL}')
-
+    
         keyDesc = components['description'].split(" ")
         keywords = components['name'].split('-')
-
+    
         # check if keyDesc is a list or not for appending to keywords
         if isinstance(keyDesc, list):
             keywords = keywords + keyDesc
         else:
             keywords.append(keyDesc)
-
+    
         keyDesc = components['description'].replace(' ', '_')
+    
         # Define the base URL for querying vulnerabilities
         base_url = "https://services.nvd.nist.gov/rest/json/cves/2.0"
-
+    
+        count = 0
         cve_found = False  # Flag to check if CVEs were found
-
+    
         keyCnt = 0
         originalCnt = len(keywords)
         keywords.append(keyDesc)
-        
-        # go through each keyword to search for possible CVE's
+    
         for keyword in keywords:
-            # keep track of which keywords are for what purpose
+    
             if originalCnt == keyCnt:
                 searchByKeyword = False
+    
             keyCnt += 1
-
+    
             if keyword.lower() == supplier.lower():
                 # Skip keyword search when it's the same as the supplier
                 continue
-
+    
             # If no CVEs were found for any keyword, you can use different parameters for requests and keywords
             if searchByKeyword:
                 # Search by keyword in the description of CVE
                 params = {'keywordSearch': f"{keyword}"}
-
+    
             else:
-                # search by vendor and component name
                 supplier_name = supplier.split(" ")
                 params = {'cpeName': f"cpe:2.3:h:{supplier_name[0]}:{keyword}:-:*:*:*:*:*:*:*"}
-
+    
             time.sleep(1)
             response = requests.get(base_url, params=params)
-
+    
             if response.status_code == 200:
                 data = response.json()
                 totalResults = data.get('totalResults', 0)
                 if 0 < totalResults < 10:
-                    
                     cve_found = True  # Set the flag to True
                     keywordValid = True
-                    st.write("")
+                    st.write()
                     cve_entries = data.get("vulnerabilities", {})
-
-                    # go through each cve found 
+    
                     for entry in cve_entries:
                         cve_id = entry.get('cve', {}).get('id', "")
                         cve_descriptions = entry.get('cve', {}).get('descriptions', {})
@@ -84,9 +81,8 @@ def process_hbom(json_data):
                             cve_cvss = entry.get('cve', {}).get('metrics', {}).get('cvssMetricV31', {})[0]
                             exploitScore = cve_cvss['exploitabilityScore']
                             impactScore = cve_cvss['impactScore']
-
                             cve_scores = f'Exploitability Score: {exploitScore}  Impact Score: {impactScore}'
-
+    
                         if searchByKeyword:
                             # Create a regular expression pattern that matches the keyword as a whole word
                             pattern = re.compile(r'\b' + re.escape(keyword) + r'\b', re.IGNORECASE)
@@ -94,21 +90,18 @@ def process_hbom(json_data):
                             description_textEng = cve_descriptions[0].get('value')
                             if not re.search(pattern, description_textEng):
                                 keywordValid = False
-
+    
                         if keywordValid:
                             if cve_id and cve_descriptions:
                                 # Print CVE details
                                 st.write(f'CVE: {cve_id}')
-
                                 st.write(f'nvd@nist.gov CVE Scores: {cve_scores}')
-
-                                # Print all references for all CVEs
+    
                                 for reference in cve_references:
                                     reference_url = reference.get("url", "")
                                     reference_source = reference.get("source", "")
                                     st.write(f'References - Source: {reference_source}, URL: {reference_url}')
-
-                                # Print CVE descriptions in languages available
+    
                                 for description in cve_descriptions:
                                     description_text = description.get('value', "")
                                     description_lang = description.get('lang', "")
@@ -116,7 +109,7 @@ def process_hbom(json_data):
                             elif cve_id and not cve_descriptions:
                                 # Print CVE without descriptions
                                 st.write(f'CVE: {cve_id}')
-
+    
                             if cve_weaknesses:
                                 for cve_cwe in cve_weaknesses:
                                     cwe_source = cve_cwe.get('source', "")
@@ -126,19 +119,19 @@ def process_hbom(json_data):
                                             cwe_name = cwe.get('value', "")
                                             # Print CWE information
                                             st.write(f'CWE for {cve_id}: {cwe_name}, Source: {cwe_source}')
-
-                    st.write("")
-
-            if cve_found:
-                break  # Exit the loop as soon as CVEs are found
-
+    
+                            st.write()
+    
+                    if cve_found:
+                        break  # Exit the loop as soon as CVEs are found
+    
         if cve_found:
             # If CVEs were found, you can continue with other components
             continue
-            
+    
         if not cve_found and not searchByKeyword:
-            st.write("### NO CVEs FOUND")
-            st.write("")
+            st.write("NO CVEs FOUND")
+            st.write()
             
 st.title("HBOM Component Processing")
 
