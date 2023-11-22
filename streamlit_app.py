031fe9b79f43a2758c7eb8476cf094dd2f5ed61f
Christopher Alvarado <104535558+ChrisA1108@users.noreply.github.com>
2023-11-22 10:48:36 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,151 +4,164 @@ import time
 import streamlit as st
 import json
 
-import json
-import streamlit as st
-import requests
-import re
-import time
+# Global variables to store CVE information
+global_cve_data = []
+cve_found = False
 
+def cve_lookup(searchByKeyword, keyword, supplier):
+    global global_cve_data, cve_found
+
+    global_cve_data = []
+
+    base_url = "https://services.nvd.nist.gov/rest/json/cves/2.0"
+
+    if searchByKeyword:
+        # Search by keyword in the description of CVE
+        params = {'keywordSearch': f"{keyword}"}
+
+    else:
+        supplier_name = supplier.split(" ")
+        params = {'cpeName': f"cpe:2.3:h:{supplier_name[0]}:{keyword}:-:*:*:*:*:*:*:*"}
+
+    apiKey = {'apiKey': 'e4b22778-a67d-4996-b3ff-ad31ead6d73d'}
+    response = requests.get(base_url, params=params, headers=apiKey)
+
+    if response.status_code == 200:
+        data = response.json()
+        totalResults = data.get('totalResults', 0)
+        if 0 < totalResults < 10:
+            cve_found = True  # Set the flag to True
+            keywordValid = True
+            cve_entries = data.get("vulnerabilities", {})
+
+            for entry in cve_entries:
+                cve_id = entry.get('cve', {}).get('id', "")
+                cve_descriptions = entry.get('cve', {}).get('descriptions', {})
+                cve_weaknesses = entry.get('cve', {}).get('weaknesses', {})
+                cve_references = entry.get('cve', {}).get("references", {})
+
+                if searchByKeyword:
+                    # Create a regular expression pattern that matches the keyword as a whole word
+                    pattern = re.compile(r'\b' + re.escape(keyword) + r'\b', re.IGNORECASE)
+                    # get cve description
+                    description_textEng = cve_descriptions[0].get('value')
+                    if not re.search(pattern, description_textEng):
+                        keywordValid = False
+
+                if keywordValid:
+                    if cve_id and cve_descriptions:
+                        # get cve metrics
+                        if entry.get('cve', {}).get('metrics', {}).get('cvssMetricV31', {}):
+                            cve_cvss = entry.get('cve', {}).get('metrics', {}).get('cvssMetricV31', {})[0]
+                            cve_metrics = cve_cvss.get('cvssData', {})
+                            exploitScore = cve_cvss['exploitabilityScore']
+                            impactScore = cve_cvss['impactScore']
+                            integrity_Impact = cve_metrics['integrityImpact']
+                            confidentiality_Impact = cve_metrics['confidentialityImpact']
+                            availability_Impact = cve_metrics['availabilityImpact']
+                            attack_vector = cve_metrics['attackVector']
+
+                        for reference in cve_references:
+                            reference_url = reference.get("url", "")
+                            reference_source = reference.get("source", "")
+
+                        cve_text = []
+                        for description in cve_descriptions:
+                            cve_text.append(description.get('value', ""))
+                            description_lang = description.get('lang', "")
+
+                    if cve_weaknesses:
+                        cwe_names = []
+                        for cve_cwe in cve_weaknesses:
+                            cwe_source = cve_cwe.get('source', "")
+                            cwe_description = cve_cwe.get('description', {})
+                            if cwe_description:
+                                for cwe in cwe_description:
+                                    cwe_name = cwe.get('value', "")
+                                    cwe_names.append(cwe_name)
+
+                    # store the CVE information in global_cve_data
+                    global_cve_data.append({
+                        'cveId': cve_id,
+                        'supplier': supplier,
+                        'cveDescription': cve_text,
+                        'referenceURL': reference_url,
+                        'cweName': cwe_names,
+                        'exploitScore': exploitScore,
+                        'impactScore': impactScore,
+                        'integrity_Impact': integrity_Impact,
+                        'confidentiality_Impact': confidentiality_Impact,
+                        'availability_Impact': availability_Impact,
+                        'attack_vector': attack_vector,
+                        # Add other relevant information as needed
+                    })
+    time.sleep(0.5)
 
 def process_hbom(HBOM):
+    global cve_found
     for components in HBOM['components']:
-        searchByKeyword = True
+        searchByKeyword = True  # start by search
+        cve_found = False # Flag to check if CVEs were found
+        keyCnt = 0  # keep track of what keyword
+
         supplier = components.get('supplier', {}).get('name', '')
         description = components['description']
-    
-        # Print supplier and description
         st.write(f'# {supplier}: {description}')
         if 'externalReferences' in components:
-            # Check if the component has external references
             for i in components['externalReferences']:
                 referenceURL = i['url']
-    
             st.write(f'referenceURL: {referenceURL}')
-    
+
         keyDesc = components['description'].split(" ")
         keywords = components['name'].split('-')
         keywords.insert(0, components['description'])
-    
-        # check if keyDesc is a list or not for appending to keywords
+
         if isinstance(keyDesc, list):
             keywords = keywords + keyDesc
         else:
             keywords.append(keyDesc)
-    
+
         keyDesc = components['description'].replace(' ', '_')
-    
-        # Define the base URL for querying vulnerabilities
-        base_url = "https://services.nvd.nist.gov/rest/json/cves/2.0"
-    
-        count = 0
-        cve_found = False  # Flag to check if CVEs were found
-    
-        keyCnt = 0
+
         originalCnt = len(keywords)
         keywords.append(keyDesc)
-    
+
         for keyword in keywords:
-    
+            #print("loop")
             if originalCnt == keyCnt:
                 searchByKeyword = False
-    
+
             keyCnt += 1
-    
+
             if keyword.lower() == supplier.lower():
-                # Skip keyword search when it's the same as the supplier
                 continue
-    
-            # If no CVEs were found for any keyword, you can use different parameters for requests and keywords
+
             if searchByKeyword:
-                # Search by keyword in the description of CVE
-                params = {'keywordSearch': f"{keyword}"}
-    
+                cve_lookup(searchByKeyword, keyword, supplier)
             else:
-                supplier_name = supplier.split(" ")
-                params = {'cpeName': f"cpe:2.3:h:{supplier_name[0]}:{keyword}:-:*:*:*:*:*:*:*"}
-    
-            time.sleep(1)
-            response = requests.get(base_url, params=params)
-    
-            if response.status_code == 200:
-                data = response.json()
-                totalResults = data.get('totalResults', 0)
-                if 0 < totalResults < 10:
-                    cve_found = True  # Set the flag to True
-                    keywordValid = True
-                    st.write()
-                    cve_entries = data.get("vulnerabilities", {})
-    
-                    for entry in cve_entries:
-                        cve_id = entry.get('cve', {}).get('id', "")
-                        cve_descriptions = entry.get('cve', {}).get('descriptions', {})
-                        cve_weaknesses = entry.get('cve', {}).get('weaknesses', {})
-                        cve_references = entry.get('cve', {}).get("references", {})
-    
-                        if searchByKeyword:
-                            # Create a regular expression pattern that matches the keyword as a whole word
-                            pattern = re.compile(r'\b' + re.escape(keyword) + r'\b', re.IGNORECASE)
-                            # get cve description
-                            description_textEng = cve_descriptions[0].get('value')
-                            if not re.search(pattern, description_textEng):
-                                keywordValid = False
-    
-                        if keywordValid:
-                            if cve_id and cve_descriptions:
-                                # Print CVE details
-                                st.write(f'## CVE: {cve_id}')
-                                if entry.get('cve', {}).get('metrics', {}).get('cvssMetricV31', {}):
-                                    cve_cvss = entry.get('cve', {}).get('metrics', {}).get('cvssMetricV31', {})[0]
-                                    cve_metrics = cve_cvss.get('cvssData', {})
-                                    exploitScore = cve_cvss['exploitabilityScore']
-                                    impactScore = cve_cvss['impactScore']
-                                    integrity_Impact = cve_metrics['integrityImpact']
-                                    confidentiality_Impact = cve_metrics['confidentialityImpact']
-                                    availability_Impact = cve_metrics['availabilityImpact']
-                                    attack_vector = cve_metrics['attackVector']
-                                    st.write("### NVD CVE Scores:")
-                                    st.write(f'Exploitability Score: {exploitScore}  Impact Score: {impactScore}')
-                                    st.write(f'Attack Vector: {attack_vector}')
-                                    st.write(f'Confidentiality Impact: {confidentiality_Impact}, Integrity Impact: {integrity_Impact}, Availability Impact: {availability_Impact}')
+                cve_lookup(searchByKeyword, keyword, supplier)
 
-                                st.write("### CVE Sources:")
-                                for reference in cve_references:
-                                    reference_url = reference.get("url", "")
-                                    reference_source = reference.get("source", "")
-                                    st.write(f'References: \nSource: {reference_source}\nURL: {reference_url}')
+            if cve_found:
+                break
 
-                                st.write("### CVE Descriptions:")
-                                for description in cve_descriptions:
-                                    description_text = description.get('value', "")
-                                    description_lang = description.get('lang', "")
-                                    st.write(f'CVE description in {description_lang}: {description_text}')
-                            elif cve_id and not cve_descriptions:
-                                # Print CVE without descriptions
-                                st.write(f'CVE: {cve_id}')
-    
-                            if cve_weaknesses:
-                                for cve_cwe in cve_weaknesses:
-                                    cwe_source = cve_cwe.get('source', "")
-                                    cwe_description = cve_cwe.get('description', {})
-                                    if cwe_description:
-                                        for cwe in cwe_description:
-                                            cwe_name = cwe.get('value', "")
-                                            # Print CWE information
-                                            st.write(f'### CWE for {cve_id}: {cwe_name}')
-    
-                            st.write()
-    
-                    if cve_found:
-                        break  # Exit the loop as soon as CVEs are found
-    
+        # return CVE details
         if cve_found:
-            # If CVEs were found, you can continue with other components
-            continue
-    
+            # Access global_cve_data after processing
+            for cve_entry in global_cve_data:
+                # Print cve name
+                st.write(f"CVE: {cve_entry['cveId']}")
+                # Print cve description
+                st.write(f"CVE Description: {cve_entry['cveDescription'][0]}")
+                # Print cvw
+                for cwe in cve_entry['cweName']:
+                    st.write(f"CWE: {cwe}")
+
         if not cve_found and not searchByKeyword:
             st.write("NO CVEs FOUND")
             st.write()
+
+
+
             
 st.title("HBOM Component Processing")
 
